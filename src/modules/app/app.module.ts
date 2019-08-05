dedf0d747faa8541b93594ce28defcef279cdef9
anasceym <anasfirdauss@gmail.com>
2019-08-05 20:14:50 +0800

    Added logger ping to payment

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -1,13 +1,39 @@
-import { Module } from '@nestjs/common'
+import { HttpModule, HttpService, Inject, Module } from '@nestjs/common'
+import * as bunyan from 'bunyan'
 
+import { ConfigModule } from '../config/config.module'
+import { CONFIG } from '../config/config.provider'
+import { Config } from '../config/interface/config.interface'
 import { DatabaseModule } from '../database/database.module'
+import { LOGGER, loggerProvider } from '../logger/logger.service'
 import { OrderModule } from '../order/order.module'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 
 @Module({
-  imports: [DatabaseModule, OrderModule],
+  imports: [DatabaseModule, OrderModule, HttpModule, ConfigModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, loggerProvider],
 })
-export class AppModule {}
+export class AppModule {
+  constructor (
+    private readonly httpService: HttpService,
+    @Inject(CONFIG) private config: Config,
+    @Inject(LOGGER) private logger: bunyan,
+  ) {
+    httpService
+      .get(
+        `http://${this.config.services.payment.host}:${
+          this.config.services.payment.port
+        }/health`,
+      )
+      .subscribe(
+        res => {
+          this.logger.info({ ...res.data }, 'Successfully ping payment-service')
+        },
+        error => {
+          this.logger.error(error, 'Unable to ping payment-service')
+        },
+      )
+  }
+}
